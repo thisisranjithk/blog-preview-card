f3844f73333fa44640e4c5fa595f4ba437b6e00d
Ranjith K <thisisranjithk@gmail.com>
2024-12-17 13:37:38 +0530

    initial commit with tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#F4D04E",
+        darkGrey: "#111111",
+        lightGrey: "#6B6B6B",
+      },
+    },
+  },
+  plugins: [],
+};
