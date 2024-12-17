e35fe5a5fdae4dd4caf36d1523be2c3e4b06caee
Ranjith K <thisisranjithk@gmail.com>
2024-12-17 16:06:54 +0530

    final commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js}"],
+  content: ["./**/*.html"],
   theme: {
     extend: {
       colors: {
         primary: "#F4D04E",
         darkGrey: "#111111",
         lightGrey: "#6B6B6B",
+      },
+      fontFamily: {
+        sans: ["FigtreeMedium", "sans-serif"],
       },
     },
   },
